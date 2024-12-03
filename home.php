d81f2800fc66cca3448ce80c550fa14dce07b039
LuisRSoto <86444761+LuisRSoto@users.noreply.github.com>
2024-12-03 06:58:27 -0400

    Update home.php

--- home.php
+++ home.php
@@ -54,7 +54,7 @@ if(isset($_POST['add_to_cart'])){
    <div class="content">
       <h3>Hand Picked Book to your door.</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quod? Reiciendis ut porro iste totam.</p>
-      <a href="about.php" class="white-btn">discover more</a>
+      <a href="about.php" class="white-btn">más información</a>
    </div>
 
 </section>
